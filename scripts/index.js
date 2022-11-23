940663a65c9fcb2e041368c5ae26b47bc350d3fd
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-11-23 22:24:34 +0100

    oo

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,50 @@
 const gallery = document.getElementById("gallery");
 
-galleryIsFocused = true;
+var lastClickedRadio = document.getElementById("radioWelcome");
 
 window.onmousemove = e => {
-    if(galleryIsFocused)
-    {
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
-    
-        const xDecimal = mouseX / window.innerWidth;
-        const yDecimal = mouseY / window.innerHeight;
-    
-        const maxX = gallery.offsetWidth - window.innerWidth;
-        const maxY = gallery.offsetHeight - window.innerHeight;
-    
-        const panX = maxX * xDecimal * -1;
-        const panY = maxY * yDecimal * -1;
-    
-        gallery.animate({
-            transform: `translate(${panX}px, ${panY}px)`
-        }, {
-            duration: 4500,
-            fill: "forwards",
-            easing: "ease"
-        })
-    }
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const xDecimal = mouseX / window.innerWidth;
+    const yDecimal = mouseY / window.innerHeight;
+
+    const maxX = gallery.offsetWidth - window.innerWidth;
+    const maxY = gallery.offsetHeight - window.innerHeight;
+
+    const panX = maxX * xDecimal * -1;
+    const panY = maxY * yDecimal * -1;
+
+    gallery.animate({
+        transform: `translate(${panX}px, ${panY}px)`
+    }, {
+        duration: 4500,
+        fill: "forwards",
+        easing: "ease"
+    })
 }
 
 function Content(value) {
+    lastClickedRadio.checked = true;
     const content = document.getElementById("content");
     if(value == "show")
     {
-        content.style.transition = "all 1s"
-        content.style.opacity = 1;
-        content.style.visibility = "visible";
+        delay(250).then(() => {
+            content.style.transition = "all 800ms"
+            content.style.opacity = 1;
+            content.style.visibility = "visible";
+        })
     }
     if(value == "hide")
     {
-        content.style.transition = "all 1s"
         content.style.opacity = 0;
         content.style.visibility = "collapse";
-        delay(1000).then(() => document.getElementById("radioContent").checked = true);
+        delay(1000).then(() => document.getElementById("radioWelcome").checked = true);
     }
 }
 
 function handleRadio(radio) {
-    console.log(radio.value)
+    lastClickedRadio = radio;
 }
 
 function delay(time) {
